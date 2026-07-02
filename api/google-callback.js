90c64c2f30b9990b9c2c24ae9071db0b2729e7d5
leobax01-dev <leobax01@gmail.com>
2026-07-02 16:43:28 -0400

    Update google-callback.js

--- api/google-callback.js
+++ api/google-callback.js
@@ -102,7 +102,7 @@ export default async function handler(req, res) {
     }
 
     res.redirect(302,
-      `${appUrl}?google_connected=true&google_email=${encodeURIComponent(googleEmail)}`
+      `${appUrl}?google_connected=true&google_email=${encodeURIComponent(googleEmail)}&screen=login`
     );
 
   } catch (err) {
